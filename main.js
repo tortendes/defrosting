1799578c4c5fa6bb684e68e81dd51edd0cb8a7e5
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2023-08-27 22:23:28 +0800

    Update 0.1

--- main.js
+++ main.js
@@ -16,21 +16,11 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  if (days < 10) {
-    days = '0' + days
+  function format(t) {
+    return t < 10 ? '0' + t : t
   }
-  if (hours < 10) {
-    hours = '0' + hours
-  }
-  if (minutes < 10) {
-    minutes = '0' + minutes
-  }
-  if (seconds < 10) {
-    seconds = '0' + seconds
-  }
-
   // Display the result in the element with id="demo"
-  document.getElementById("defrost").innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+  document.getElementById("defrost").innerHTML = `${format(days)}:${format(hours)}:${format(minutes)}:${format(seconds)}`;
 
   // If the count down is finished, write some text
   if (distance < 0) {
